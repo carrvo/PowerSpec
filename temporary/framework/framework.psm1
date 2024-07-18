bbf8ae5b89ad99dc0ef392bbdeb9175965cb74b7
carrvo <carrvo@gmail.com>
2024-07-18 11:45:34 -0600

    returns SpecificationResult and includes Import/Export

--- temporary/framework/framework.psm1
+++ temporary/framework/framework.psm1
@@ -1,6 +1,12 @@
 using namespace System.Management.Automation
 
-function Invoke-Specification {
+class SpecificationResult {
+	[PSCustomObject] $ModuleId
+	[PSCustomObject] $TypeId
+	[PSCustomObject[]] $TestResultIds
+}
+
+function Test-Specification {
 	[CmdletBinding()]
 	Param(
 		[Parameter(Mandatory)]
@@ -21,5 +27,37 @@ function Invoke-Specification {
 			ForEach-Object { $specMap.$_ } |
 			ForEach-Object { @{ Path = "$base\$_"; Parameters = @{ Instance = $Instance } } }
 		$results = Invoke-Pester -Script $specTests -PassThru
+		$specResult = [SpecificationResult]@{
+			ModuleId = $API | Select-Object -Property CompanyName,ModuleBase,Name,Version,Guid
+			TypeId = $testType | Select-Object -Property @{Name='Assembly';Expression={$_.Assembly.FullName}},Namespace,Name,AssemblyQualifiedName,GUID
+			TestResultIds = $results.TestResult | Select-Object -Property Describe,Context,Name,ParameterizedSuiteName,Parameters,Result,Passed,FailureMessage
+		}
+		Write-Output $specResult
+	}
+}
+
+function Export-SpecificationResult {
+	[CmdletBinding()]
+	Param(
+		[Parameter(Mandatory)]
+		[SpecificationResult[]] $SpecificationResults,
+		
+		[Parameter(Mandatory)]
+		[ValidatePattern('.json$')]
+		[System.IO.FileInfo] $Path
+	)
+	Process {
+		ConvertTo-Json -InputObject $SpecificationResults -Depth 4 | Out-File -LiteralPath $Path.FullName
+	}
+}
+
+function Import-SpecificationResult {
+	[CmdletBinding()]
+	Param(
+		[Parameter(Mandatory)]
+		[System.IO.FileInfo] $Path
+	)
+	Process {
+		Get-Content -LiteralPath $Path | ConvertFrom-Json | Write-Output | ForEach-Object {[SpecificationResult]$_}
 	}
 }
