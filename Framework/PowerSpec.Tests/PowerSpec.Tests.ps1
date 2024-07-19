89086a8addb10909a79cfff31d061f28e9f641ad
carrvo <carrvo@gmail.com>
2024-07-18 22:07:33 -0600

    reformat project

---
+++ Framework/PowerSpec.Tests/PowerSpec.Tests.ps1
@@ -0,0 +1,18 @@
+﻿Describe "PowerSpec" {
+	Import-Module "$PSScriptRoot\..\PowerSpec","$PSScriptRoot\..\..\ContrivedExample\ApplicationSpecification","$PSScriptRoot\..\..\ContrivedExample\ThirdPartyPowerShell"
+	$specResult = Test-Specification -API (Get-Module ApplicationSpecification) -Instance (Get-MyList)
+	
+	Context "Test Results" {
+		It "identifies the API" {
+			$specResult.ModuleId.Name | Should Be ApplicationSpecification
+		}
+		
+		It "identifies the instance Type" {
+			$specResult.TypeId.Name | Should Be MyList
+		}
+		
+		It "should pass specification (all tests)" {
+			$specResult.TestResultIds.Passed | Should Be $true
+		}
+	}
+}
