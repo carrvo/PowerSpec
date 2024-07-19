79bf5c8166c6b09ea1c3116525f7218ac741ef5a
carrvo <carrvo@gmail.com>
2024-07-18 22:08:47 -0600

    cannot run Pester inside Pester

--- Framework/PowerSpec.Tests/PowerSpec.Tests.ps1
+++ Framework/PowerSpec.Tests/PowerSpec.Tests.ps1
@@ -1,7 +1,7 @@
-﻿Describe "PowerSpec" {
-	Import-Module "$PSScriptRoot\..\PowerSpec","$PSScriptRoot\..\..\ContrivedExample\ApplicationSpecification","$PSScriptRoot\..\..\ContrivedExample\ThirdPartyPowerShell"
-	$specResult = Test-Specification -API (Get-Module ApplicationSpecification) -Instance (Get-MyList)
-	
+﻿Import-Module "$PSScriptRoot\..\PowerSpec","$PSScriptRoot\..\..\ContrivedExample\ApplicationSpecification","$PSScriptRoot\..\..\ContrivedExample\ThirdPartyPowerShell"
+$specResult = Test-Specification -API (Get-Module ApplicationSpecification) -Instance (Get-MyList)
+
+Describe "PowerSpec" {
 	Context "Test Results" {
 		It "identifies the API" {
 			$specResult.ModuleId.Name | Should Be ApplicationSpecification
